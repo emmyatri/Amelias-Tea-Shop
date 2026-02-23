0cb7fbba446568b120a7cd250f0c6774f00bf6e9
emmyatri <aelling6@students.kennesaw.edu>
2026-02-23 14:20:26 -0500

    Add InventoryQueryOutput

--- src/TeaShop/TeaShop/UserInterface/QueryBuilder/InventoryQueryOutput.cs
+++ src/TeaShop/TeaShop/UserInterface/QueryBuilder/InventoryQueryOutput.cs
@@ -2,6 +2,9 @@
 
 namespace TeaShop.UserInterface.QueryBuilder;
 
-public class InventoryQueryOutput(List<QueriedInventoryItem> items, List<string> appliedFilter)
+public class InventoryQueryOutput(IReadOnlyList<QueriedInventoryItem> items, IReadOnlyList<string> appliedFilters)
 {
+    public IReadOnlyList<QueriedInventoryItem> Items { get; } = items;
+    public IReadOnlyList<string> AppliedFilter { get; } = appliedFilters;
+    
 }
