75bfef1044996e83350bf14f30e336972df2544d
emmyatri <aelling6@students.kennesaw.edu>
2026-02-25 19:01:00 -0500

    refactor: move PaymentBuilderListFactory to composition root, inject into PaymentHandler

--- src/TeaShop/TeaShop/Program.cs
+++ src/TeaShop/TeaShop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using TeaShop.Domain.Inventory;
 using TeaShop.UserInterface;
+using TeaShop.UserInterface.PaymentBuilder;
 using TeaShop.UserInterface.PurchaseBuilder;
 using TeaShop.UserInterface.QueryBuilder;
 
@@ -9,7 +10,8 @@ var repository = new InventoryRepository();
 var input = new UserPrompt(Console.In, Console.Out);
 var queryBuilder = new InventoryQueryBuilder(input, repository);
 var outputWriter = new InventoryQueryOutputWriter(Console.Out);
-var paymentHandler = new PaymentHandler(input, Console.Out);
+var paymentMethods = new PaymentBuilderListFactory().Create(input, Console.Out);
+var paymentHandler = new PaymentHandler(input, paymentMethods, Console.Out);
 var purchaseHandler = new PurchaseHandler(input, paymentHandler, repository, Console.Out);
 var app = new Application(input, queryBuilder, outputWriter, purchaseHandler, Console.Out);
 
--- src/TeaShop/TeaShop/UserInterface/PurchaseBuilder/PaymentHandler.cs
+++ src/TeaShop/TeaShop/UserInterface/PurchaseBuilder/PaymentHandler.cs
@@ -8,26 +8,25 @@ namespace TeaShop.UserInterface.PurchaseBuilder;
 ///     Displays available payment methods, collects the user's selection,
 ///     and delegates checkout to the chosen payment builder.
 /// </summary>
-public sealed class PaymentHandler(UserPrompt reader, TextWriter writer)
+public sealed class PaymentHandler(UserPrompt reader, IReadOnlyList<IPaymentBuilder> paymentMethods, TextWriter writer)
 {
     private readonly UserPrompt _reader = reader ?? throw new ArgumentNullException(nameof(reader));
+    private readonly IReadOnlyList<IPaymentBuilder> _paymentMethods = paymentMethods ?? throw new ArgumentNullException(nameof(paymentMethods));
     private readonly TextWriter _writer = writer ?? throw new ArgumentNullException(nameof(writer));
 
 
     public void ProcessPayment(decimal totalPrice, string itemName, int quantity)
     {
-        var factory = new PaymentBuilderListFactory();
-        var builder = factory.Create(_reader, _writer);
-
+        
         _writer.WriteLine();
         _writer.WriteLine("*** Choose a payment method: ");
-        for (var i = 0; i < builder.Count; i++) _writer.WriteLine($"{i + 1}. {builder[i].Name}");
+        for (var i = 0; i < _paymentMethods.Count; i++) _writer.WriteLine($"{i + 1}. {_paymentMethods[i].Name}");
         _writer.WriteLine();
 
 
-        var selection = _reader.ReadInt("Selection: ", 1, 1, builder.Count);
+        var selection = _reader.ReadInt("Selection: ", 1, 1, _paymentMethods.Count);
         var purchase = new PurchaseDetails(totalPrice, itemName, quantity);
-        var strategy = builder[selection - 1].Build(purchase);
+        var strategy = _paymentMethods[selection - 1].Build(purchase);
         _writer.WriteLine(strategy.Checkout());
     }
 }
